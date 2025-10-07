9f0398046eecd4f03aafc7db7fea27c244105a4f
Perparimpepe <perparimpepe@gmail.com>
2025-10-07 15:20:30 +0300

    h

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const ejs = require('ejs');
 const fs = require('fs');
 const path = require('path');
 
+
 // Middleware
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); // Directory
