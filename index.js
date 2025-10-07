9eca9fa0262a1802d0d444251d822249d1f4f858
Perparimpepe <perparimpepe@gmail.com>
2025-10-07 15:54:51 +0300

    ok

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require('path');
 // Middleware
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); // Directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
